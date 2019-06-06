04456b911e22e22deeb9a50f68c9584b475608c1
Hayk232002 <hayknazaryan10@gmail.com>
2019-06-06 23:26:08 +0400

    Cod

--- app/src/main/java/com/example/lynx1/Login.java
+++ app/src/main/java/com/example/lynx1/Login.java
@@ -2,9 +2,13 @@ package com.example.lynx1;
 
 import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.constraint.Guideline;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -35,7 +39,11 @@ public class Login extends AppCompatActivity {
     TextView tv;
     TextView tv_forgot;
     Button btn_signIn_login;
+    Guideline gl_et_password;
+    Guideline gl_btn_login;
+    Guideline gl_tv_forgot;
     ConstraintLayout cl_login;
+    ConstraintSet constraintSet;
 
     ObjectAnimator objectAnimator_tv_forgot_gnal;
     ObjectAnimator objectAnimator_btn_login_gnal;
@@ -47,6 +55,9 @@ public class Login extends AppCompatActivity {
     boolean getCoordinates = false;
     float btn_signIn_login_y = 0;
     float tv_forgot_y = 0;
+    float gl_et_password_y = 0;
+    float gl_btn_login_y = 0;
+    float gl_tv_forgot_y = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +70,9 @@ public class Login extends AppCompatActivity {
         btn_signIn_login = (Button) findViewById(R.id.btn_signIn_login);
         et_email_login = (EditText) findViewById(R.id.et_email_login);
         cl_login = (ConstraintLayout) findViewById(R.id.cl_login);
+        gl_et_password = (Guideline) findViewById(R.id.gl_et_password);
+        gl_btn_login = (Guideline) findViewById(R.id.gl_btn_login);
+        gl_tv_forgot = (Guideline) findViewById(R.id.gl_tv_forgot);
 
         //Firebase
         mAuth = FirebaseAuth.getInstance();
@@ -71,7 +85,7 @@ public class Login extends AppCompatActivity {
             }
         });
 
-                btn_signIn_login.setOnClickListener(new View.OnClickListener() {
+        btn_signIn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 singing(et_email_login.getText().toString(),et_password_login.getText().toString());
@@ -84,12 +98,16 @@ public class Login extends AppCompatActivity {
 
             }
 
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
                 if (!getCoordinates){
                     btn_signIn_login_y = btn_signIn_login.getY();
                     tv_forgot_y = tv_forgot.getY();
+                    gl_et_password_y = gl_et_password.getY();
+                    gl_btn_login_y = gl_btn_login.getY();
+                    gl_tv_forgot_y = gl_tv_forgot.getY();
 
                     getCoordinates = true;
                 }
@@ -97,8 +115,8 @@ public class Login extends AppCompatActivity {
                 if(charSequence.toString().trim().length()!=0 && count==0){
                     Log.wtf("gnal:",charSequence.toString());
 
-                    objectAnimator_tv_forgot_gnal=ObjectAnimator.ofFloat(tv_forgot,"y",1050);
-                    objectAnimator_btn_login_gnal=ObjectAnimator.ofFloat(btn_signIn_login,"y",910);
+                    objectAnimator_tv_forgot_gnal=ObjectAnimator.ofFloat(tv_forgot,"y",/*1050*/ gl_tv_forgot_y);
+                    objectAnimator_btn_login_gnal=ObjectAnimator.ofFloat(btn_signIn_login,"y",/*910*/ gl_btn_login_y);
 
                     objectAnimator_btn_login_gnal.setDuration(350);
                     objectAnimator_tv_forgot_gnal.setDuration(350);
@@ -106,11 +124,13 @@ public class Login extends AppCompatActivity {
                     objectAnimator_btn_login_gnal.start();
 
                     et_password_login = new EditText(Login.this);
-                    ViewGroup.LayoutParams layoutParams =(ViewGroup.LayoutParams) et_email_login.getLayoutParams();
 
+                    ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(et_email_login.getWidth(), et_email_login.getHeight());
+
+                    et_password_login.setId(EditText.generateViewId());
                     et_password_login.setLayoutParams(layoutParams);
-                    et_password_login.setX(0);
-                    et_password_login.setY(150);
+//                    et_password_login.setX(0);
+//                    et_password_login.setY(gl_et_password_y);
                     et_password_login.setPadding(et_email_login.getPaddingLeft(),et_email_login.getPaddingTop(),et_email_login.getPaddingRight(),et_email_login.getPaddingBottom());
                     et_password_login.setBackgroundResource(R.drawable.rounded_edittect_registr_login);
                     et_password_login.setHint("Password");
@@ -118,6 +138,17 @@ public class Login extends AppCompatActivity {
 //                    et_password_login.setTransformationMethod(PasswordTransformationMethod.getInstance());
 
                     cl_login.addView(et_password_login);
+
+                    constraintSet = new ConstraintSet();
+                    constraintSet.clone(cl_login);
+
+//                    constraintSet.connect(et_password_login.getId(),ConstraintSet.START,cl_login.getId(),ConstraintSet.START,0);
+                    constraintSet.connect(et_password_login.getId(),ConstraintSet.TOP,et_email_login.getId(),ConstraintSet.BOTTOM);
+                    constraintSet.setMargin(et_password_login.getId(),ConstraintSet.TOP,16);
+//                    constraintSet.connect(et_password_login.getId(),ConstraintSet.BOTTOM,cl_login.getId(),ConstraintSet.BOTTOM,0);
+//                    constraintSet.connect(et_password_login.getId(),ConstraintSet.END,cl_login.getId(),ConstraintSet.END,0);
+                    constraintSet.constrainDefaultHeight(et_password_login.getId(), 200);
+                    constraintSet.applyTo(cl_login);
 
                     count=1;
                 }
